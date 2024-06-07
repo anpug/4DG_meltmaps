0772ff1b233d259ecb063ba754556128d0fa1c43
annpu <annpu@space.dtu.dk>
2024-06-07 13:05:49 +0200

    updated scripts

--- scripts/helper_tools.py
+++ scripts/helper_tools.py
@@ -6,122 +6,112 @@ Created on Tue Aug 22 11:38:49 2023
 @author: annpu
 """
 
-import numpy as np 
+from scipy.interpolate import griddata
+import numpy as np
+import cartopy
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import cartopy.feature
+import matplotlib.colors as colors
+import seaborn as sns
 
 
+
+def grid_data(lat_ori, lon_ori, var_ori, ref_lat, ref_lon, method_name):
+    
+    '''
+    Function used to interpolate/regrid melt data to the ASCAT grid (in this case). Important that the original 
+    grid and new grid is in the same projection (in this case EPSG:3413)
+    
+    Input: lat_ori, lon_ori: coordinates of the original grid (2D arrays)
+           var_ori: Data to be regridded on the original grid (2D arrays).
+           ref_lon, ref_lat: 
+           method: 
+    
+    Output: (melt)Data on the new grid. 
+    '''
+   
+    grid_var = griddata((lon_ori.flatten(), lat_ori.flatten()), var_ori.flatten(), (ref_lon, ref_lat), method=method_name)
+
+    return grid_var
 
 def number_of_meltdays(rcm, threshold): 
     '''
     Compute the number of meltdays in a year for a RCM. Meltdays are thresholded by a magnitude threshold only. 
     '''    
     melt = np.zeros(rcm.shape)
-    #melt = np.full([365, 540, 299], np.nan)
     melt[rcm >= threshold] = 1
     sum_meltdays = np.sum(melt, axis = 0)
     sum_meltdays[sum_meltdays == 0] = np.nan
     return sum_meltdays 
 
-
-        
-def rolling_window(a, size):
+def compute_melt_extent(rcm, threshold): 
     '''
-    Takes an array (1D or 2D) and compute an array using a rolling window (2D or 3D array). The rollig array is 
-    created along the first axis (i.e. time). 
-    
-    Input: a: The original array that the rolling array needs to be computed from. 
-           size: size of the rolling window. 
-    
-    Output: The rolling window array. 
-    '''
-    
-    shape = a.shape[:-1] + (a.shape[-1] - size + 1, size)
-    strides = a.strides + (a.strides[-1],)
-    
-    return np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides)
-
-def find_first_last(arr):
-    '''
-    Takes a 3D boolean array and finds the first and last occurence of True along the zero axis (time in this case).
-    
-    Input: arr: 3D boolean array
-    Output: first_occurrence: The index of the first True occurence. 
-            last_occurence: The index of the last True occurance
-    '''
-    
-    # Find the index of the first occurence:  
-    first_occurrence = np.argmax(arr, axis=0)
-    # Flip array around and find the last occurence. Since the index correspond to the flipped array, substract 
-    # with the size of array: 
-    last_occurrence = arr.shape[0] - np.argmax(np.flip(arr, axis=0), axis=0) - 1
-    return first_occurrence.astype(np.float32),  last_occurrence.astype(np.float32)
+    Compute the number of meltdays in a year for a RCM. Meltdays are thresholded by a magnitude threshold only. 
+    '''    
+    melt = np.zeros(rcm.shape)
+    #melt = np.full([365, 540, 299], np.nan)
+    melt[rcm >= threshold] = 1
+    melt_extent = np.sum(melt.reshape(melt.shape[0], -1), axis = 1)
+    return melt_extent/59959*100
 
 
-def melt_season(array, thresholds, length):
-    
-    '''
-    Function used to compute the start and end of the melt season. The melt season is defined using a threshold 
-    for the magnitude (either mmWE/day for RCM or class threshold for ASCAT) and a length of consective melt 
-    threshold. 
-    
-    Input: array: a year of either daily RCM active melt or ASCAT melt classification (3D array)
-           threshold: lower and upper magnitude threshold (the upper threshold for RCM should be very large as
-                      there should be no upper threshold)
-           length: Length threshold of how many days of consective melt defines the start and end of melt season. 
-    Output; Start and end of the melt season for each pixel. 
-    '''
-    
-    outarray = np.zeros_like(array).astype('bool') # Zero array 
-    subarray1 = np.ones(length) * thresholds[0] # Lower magnitude threhold
-    subarray2 = np.ones(length) * thresholds[1] # Upper magnitude threhold
-    # Compute boolean for length and magnitude thresholds is statisfied: 
-    index1 = np.all(rolling_window(array, length) >= subarray1, axis=2) # Lower magnitude threholding 
-    index2 = np.all(rolling_window(array, length) <= subarray2, axis=2) # Upper magnitude threholding 
-    index = index1 & index2 # Both magnitude thresholds 
-    # Correct indexing
-    outarray[:, :index.shape[1]] = index
-    # Find start and end: 
-    start, end = find_first_last(outarray.T)
-    start[start == 0] = np.nan # Set no start of melt season to nan 
-    start = start.reshape((540,299)) # Correct grid shape 
-    end[end >= 364] = np.nan # Set no end of melt season to nan 
-    end = end.reshape((540,299)) # Correct grid shape 
-    end = end + length - 1 # correct indexing since it has to be the last day where the thresholding was satisfied
-    return start, end
+def compute_max_melt_extent(melt_days, ascat_mask):
+    melt_days[(melt_days.astype(str)==str(np.nan)) & (ascat_mask.astype(str) != str(np.nan)) ] = 0
+    # Compute extent:
+    extent = np.zeros(melt_days.shape)
+    extent[melt_days*ascat_mask>0] = 1
+    return np.sum(extent)/66427*100
 
 
-def rolling_window_rcm(array, threshold, length):
-    outarray = np.zeros_like(array).astype('bool')
-    subarray = np.ones(length) * threshold
-    index = np.any(rolling_window(array, length) >= subarray, axis=2)
-    outarray[:, :index.shape[1]] = index
-    return outarray.T
+# For consistent plotting on Greenland
+def plot_melt_volume(data, ax, transform = cartopy.crs.epsg('3413'), mask = None, 
+                   vmin = 0, vmax = 100, 
+                   cbar = False, cbar_label = None, cmap = 'magma'): 
+    grid = xr.open_dataset(home_dir + '/tif_grid.nc')
+    grid_X, grid_Y = np.meshgrid(grid.x,grid.y)
+    
+    if mask is not None: 
+        # Mask data: _r 
+        data = data * mask 
+    
+    plot = ax.scatter(grid_X, grid_Y, c = np.flip(data+0.00001, axis =0 ), 
+               transform = transform, s = 0.35, cmap = cmap, 
+               #vmin = vmin, vmax = vmax, 
+               norm = colors.LogNorm(vmin = 0.01, vmax = 30000 )) # ,vmin= 0,vmax = 2000
+    #ax.contour(grid_X, grid_Y, np.flip(data+0.00001, axis =0 ), transform = transform, 
+    #           levels = 100, norm = colors.LogNorm( ), colors = 'black')
+    #ax.add_feature(cartopy.feature.LAND,color = 'tan', alpha = 0.7)
+    ax.add_feature(cartopy.feature.LAND,color = '#C6C7C8', alpha = 0.7)
+    
+    ax.coastlines(resolution='50m', linewidth = 0.05, color = 'black')
+    ax.gridlines(linewidth=0.8, linestyle = ':')
 
-def window_comparison(ascat, rcm, threshold, length): 
-    
-    '''
-    Compute find the ratio (procentage) between days with melt/refreezing/no melt in both ASCAT and RCM 
-    and total number of days with melt/refreezing/no melt in ASCAT. It is possible to specify a window in which 
-    the RCM "looks into the future" using the length variable. If length = 1, no window is applied. 
-    
-    Input: ascat: Boolean-like that specifies if selected ascat claasification is satisfied. 
-           rcm: RCM with daily active melt. 
-           threshold: Threshold for when a day is clssified as a melt day in the RCM. 
-           length: The number of days in the future included in the RCM used to account for the preporsseing 
-                   window of ASCAT. If length = 1, no window is applied. 
-    
-    Output: The melt/refreezing/no melt ratio for each pixel in the ascat grid. Pixels where melt/refreezing/no melt
-            is not detecetd by ASCAT is set to nan. 
-    '''
-    
-    # Computed boolean array melt occurs over a threshold inside a window of specified length: 
-    window_rcm = rolling_window_rcm(rcm.reshape((rcm.shape[0], -1)).T,threshold, length)
-    melt_rcm = window_rcm.reshape(rcm.shape) # reshape into original dimentions to match ASCAT 
-    
-    # Days where 
-    match = np.equal(ascat.astype('bool'), melt_rcm).astype('float')
-    # Exclude indexs where ASCAT is False: 
-    match[ascat == 0] = np.nan
-    # Sum days: 
-    sum_match = np.nansum(match, axis = 0)
-    sum_ascat = np.nansum(ascat, axis = 0)
-    return np.divide(sum_match,sum_ascat)*100 # Computed ratio. 
+    if cbar:
+        plt.colorbar(plot , ax = ax , label = cbar_label)
+    return plot
+
+# For consistent plotting on Greenland
+def plot_greenland(data, ax, transform = cartopy.crs.epsg('3413'), mask = None, 
+                   vmin = 0, vmax = 100, 
+                   cbar = False, cbar_label = None, cmap = sns.cubehelix_palette(start=.5, rot=-.75, as_cmap=True), text_label = None): 
+    if mask is not None: 
+        # Mask data: 
+        data = data * mask 
+    grid = xr.open_dataset(home_dir + '/tif_grid.nc')
+    grid_X, grid_Y = np.meshgrid(grid.x,grid.y)
+    plot = ax.scatter(grid_X, grid_Y, c = np.flip(data, axis =0 ), 
+               transform = transform, s = 0.5, cmap = cmap, 
+               vmin = vmin, vmax = vmax)
+    ax.add_feature(cartopy.feature.LAND,color = '#C6C7C8', alpha = 0.7)
+    ax.coastlines(resolution='10m', linewidth = 0.05, color = 'black')
+    ax.gridlines(linewidth=0.3)
+
+    if cbar:
+        plt.colorbar(plot , ax = ax , label = cbar_label)
+
+
+    if text_label is not None: 
+        ax.text(0.99, 0.01, text_label, transform=ax.transAxes, fontsize=16,  va='bottom', ha='right')
+
+    return plot
--- scripts/load_data.py
+++ scripts/load_data.py
@@ -11,10 +11,13 @@ import numpy as np
 import pyproj 
 from osgeo import gdal
 import pickle
+import geopandas as gpd
+from scipy.spatial import cKDTree
+import pandas as pd
+import rasterio as rio
+import rioxarray as rxr
 
-def preprocess_func(RCM):
-    RCM.coords['lon_2'] = (RCM.coords['lon_2'] + 180) % 360 - 180
-    return RCM
+
 
 def tranform_coord(lon, lat): 
     transformer = pyproj.Transformer.from_crs('EPSG:4326','EPSG:3413')  
@@ -67,9 +70,23 @@ def load_RCM(home_dir, RCM_name, year, season = None):
         polar_lon,polar_lat = np.meshgrid(polar_x, polar_y)
         
     elif RCM_name == 'HIRHAM': 
+        
+        # Variables not to be included in the data load:
+        varlist = ['ahfl','ahfs','dlwrad','dswrad','evspsbl','rainfall','snfall','grndhflx','tsl2','gld','rogl',
+                   'tradsm','sradsm','albedom','sn','supimp','tsl','rfrz']
         # Import nc-file: 
-        RCM =  xr.open_dataset(home_dir + f'/HIRHAM/{year}/HIRHAM_{year}.nc')
-        melt_data = RCM.snmel.data.squeeze()
+        #RCM =  xr.open_mfdataset(home_dir + f'/HIRHAM_2/Daily2D_{year}*.nc', 
+        #                        parallel=True, drop_variables = varlist,
+        #                        concat_dim='time', combine='nested')
+        #RCM =  xr.open_mfdataset(home_dir + f'/HIRHAM/{year}/Daily2D_{year}*.nc', 
+        #                        parallel=True, drop_variables = varlist,
+        #                        concat_dim='time', combine='nested')
+        # HIRHAM-ERA5_v2: 
+        RCM =  xr.open_mfdataset(home_dir + f'/Daily2D_{year}*.nc', 
+                                parallel=True, drop_variables = varlist,
+                                concat_dim='time', combine='nested')
+        
+        melt_data = RCM.tas.data.squeeze()
         
         # Get latlon coordinates:
         lon = RCM.lon.data 
@@ -78,19 +95,6 @@ def load_RCM(home_dir, RCM_name, year, season = None):
         # Transform into EPSG 3413 using pyproj: 
         polar_lon,polar_lat = tranform_coord(lon, lat)
 
-    elif RCM_name == 'CARRA': 
-        # Load in a full year of CARRA data using with lon from -180,180 degrees: 
-        RCM = xr.open_mfdataset(home_dir + f'/CARRA/Daily2D_{year}*.nc', 
-                                concat_dim = 'time' , combine = 'nested', 
-                                preprocess=preprocess_func, parallel = True, data_vars = ['snmel'])
-
-        # Extract lon og lat and reproject to North Polar Stereographic (EPSG:3431)
-        lon = RCM.lon_2.data 
-        lat = RCM.lat_2.data 
-        polar_lon,polar_lat = tranform_coord(lon, lat)
-
-        # Convert melt data to array: 
-        melt_data = np.array(RCM.snmel.data)
         
     return polar_lat, polar_lon, melt_data
 
@@ -137,13 +141,14 @@ def write_geotiff(filename, arr, in_ds):
         arr_type = gdal.GDT_Float32
     else:
         arr_type = gdal.GDT_Int32
-
+    
     driver = gdal.GetDriverByName("GTiff")
     out_ds = driver.Create(filename, arr.shape[1], arr.shape[0], 1, arr_type)
     out_ds.SetProjection(in_ds.GetProjection())
     out_ds.SetGeoTransform(in_ds.GetGeoTransform())
     band = out_ds.GetRasterBand(1)
     band.WriteArray(arr)
+    band.SetNoDataValue(np.nan)  # Set np.nan as nodata
     band.FlushCache()
     band.ComputeStatistics(False)
 
@@ -166,3 +171,233 @@ def icemask_from_tif(file_path):
     return icemask 
 
 
+def load_ASCAT_grid(home_dir): 
+    # Open nc file with the desired grid; (Made using gdal_translate)
+    tif_grid = xr.open_dataset(home_dir + '/tif_grid.nc')
+
+    # Generate ASCAT grid: 
+    tif_x = tif_grid.x
+    tif_y = tif_grid.y
+    tif_lon, tif_lat = np.meshgrid(tif_x,tif_y)
+    return tif_lat, tif_lon
+
+
+def load_dmidata(home_dir, year, rcm_name): 
+    varlist = ['ahfl','ahfs','dlwrad','dswrad','evspsbl','rainfall','snfall','grndhflx','tsl2','gld','rogl',
+           'tradsm','sradsm','albedom','supimp','tsl','rfrz']
+    if rcm_name == 'HIRHAM_ERA5': 
+            # Open and concatenate netCDF files into one dataset
+            #RCM = xr.open_mfdataset(home_dir+f'/HIRHAM_2/Daily2D_{year}*.nc',
+            #                    parallel=True, drop_variables = varlist,
+            #                    concat_dim='time', combine='nested')
+            RCM =  xr.open_mfdataset(home_dir + f'/HIRHAM5-ERA5_v2/Daily2D_{year}*.nc', 
+                                parallel=True, drop_variables = varlist,
+                                concat_dim='time', combine='nested')
+    
+    elif rcm_name == 'HIRHAM_ERAI':
+        if year >= 2015: 
+            RCM =  xr.open_mfdataset(home_dir + f'/HIRHAM/{year}/HIRHAM_{year}.nc', drop_variables = varlist)
+        
+        else:
+            RCM = xr.open_mfdataset(home_dir+f'/HIRHAM/{year}/Daily2D_{year}*.nc',
+                                parallel=True, drop_variables = varlist,
+                                concat_dim='time', combine='nested') 
+    
+    return RCM
+        
+def sample_xarray_points(RCM, aws_df): 
+    RCM_xy = RCM.sel(x = np.array(aws_df.geometry.x), y = np.array(aws_df.geometry.y), method = 'nearest')
+
+    # Nu skal det så ind i en dataframe 
+    data4df = np.zeros((np.size(RCM_xy.time), len(aws_df)))
+    for i in range(len(aws_df)):
+        # Set up data for dataframe:
+        data4df[:, i] = np.array(RCM_xy.isel(x=i, y=i).ME).squeeze()
+
+    #Put into dataframe:
+    sampled_df = pd.DataFrame(data = data4df, 
+                index = RCM_xy.time.dt.floor('d'),
+                columns = list( aws_df.index))
+
+    return sampled_df
+
+# Function that loads in RCM from one netcdf-file in a given year and season. The functions then samples the data in the AWS locations and only returns the melt data at these locations.
+def get_melt_in_points(home_dir, date_range, aws_df, rcm_name): 
+    # Create a dataframe: 
+    AWS_RCM = pd.DataFrame(columns = list(aws_df.index))
+
+    # Since MAR and RACMO data structure are in the data structure the method is very similar:
+    if rcm_name == 'MAR' or rcm_name == 'RACMO' or rcm_name == 'RACMO_s': 
+
+        for year in range(date_range[0], date_range[1]+1):
+            # Load RCM:
+            
+            if rcm_name == 'MAR':
+                RCM = xr.open_dataset(home_dir + f'/marv3.12/MARv3.12.1-10km-daily-ERA5-{year}.nc')
+                # For MAR we need to correct the xy to m:
+                RCM.coords['x'] = RCM.coords['x']*1000
+                RCM.coords['y'] = RCM.coords['y']*1000
+                # Rename TIME to time:
+                RCM = RCM.rename_vars({'TIME': 'time'})
+                sampled_df = sample_xarray_points(RCM, aws_df)
+
+            elif rcm_name == 'RACMO':
+                seasons = ['AMJ', 'JAS', 'OND']
+                RCM = xr.open_dataset(home_dir + f'/RACMO2.3/Daily-1km/snowmelt.{year}_JFM.BN_RACMO2.3p2_ERA5_3h_FGRN055.1km.DD.nc')
+                # Rename snowmeltcorr to ME: 
+                RCM = RCM.rename_vars({'snowmeltcorr': 'ME'})
+                sampled_df = sample_xarray_points(RCM, aws_df)
+                for s in seasons: # load in the rest of the seasons:
+                    RCM = xr.open_dataset(home_dir + f'/RACMO2.3/Daily-1km/snowmelt.{year}_{s}.BN_RACMO2.3p2_ERA5_3h_FGRN055.1km.DD.nc')
+                    # Rename snowmeltcorr to ME: 
+                    RCM = RCM.rename_vars({'snowmeltcorr': 'ME'})
+                    sampled_df_s = sample_xarray_points(RCM, aws_df)
+                    sampled_df = pd.concat([sampled_df, sampled_df_s])
+
+            elif rcm_name == 'RACMO_s': # only the JAS t2m data
+                
+                RCM = xr.open_dataset(home_dir + f'/RACMO2.3/t2m/t2m.{year}_JAS.BN_RACMO2.3p2_ERA5_3h_FGRN055.1km.DD.nc')
+                # remane t2m to TT: 
+                RCM = RCM.rename_vars({'t2m': 'TT'})
+                # Convert from km to m: 
+                #RCM.coords['x'] = RCM.coords['x']*1000
+                #RCM.coords['y'] = RCM.coords['y']*1000  
+                # Sample in points:
+                sampled_df = sample_xarray_points(RCM, aws_df)
+                
+
+            # concat onto dataframe:
+            AWS_RCM = pd.concat([AWS_RCM, sampled_df])
+            print(f'Dataframe updated for {year}\n')
+
+    elif rcm_name == 'HIRHAM_ERA5' or rcm_name == 'HIRHAM_ERAI': 
+       
+        # Load HIRHAM for the first year:
+        RCM = load_dmidata(home_dir, date_range[0], rcm_name)
+        
+        print('Loaded data for:', date_range[0])
+        # Convert to the correct time format: (datetime64)
+        time_index = np.arange(np.datetime64(f'{date_range[0]}-01-01'), 
+                               np.datetime64(f'{date_range[0]+1}-01-01'), 
+                               np.timedelta64(1, 'D'))
+
+        # Flatten and stack latitude and longitude values to create a 2D grid
+        lon = np.array(RCM.lon.data).flatten()
+        lat = np.array(RCM.lat.data).flatten()
+        lat_lon_grid = np.column_stack((lat, lon))
+
+        # Build a KD-tree for efficient nearest neighbor searches
+        tree = cKDTree(lat_lon_grid)
+        
+        # Extract AWS coordinates and find their nearest neighbors in the grid
+        aws_coords = np.column_stack([aws_df.lat, aws_df.lon])
+        dist , closest_indices = tree.query(aws_coords, k=1)
+
+        print('Nearest coordinates found\n')
+        # Reshape melt data for compatibility with the lat-lon grid
+        melt_data = np.array(RCM.snmel.data).reshape(RCM.snmel.data.shape[0], -1)
+        
+        # Extract melt data for the nearest grid points
+        melt_data_aws = melt_data[:, closest_indices]
+
+        # Create a DataFrame with the melt data
+        sampled_df = pd.DataFrame(data=melt_data_aws, 
+                                  index=time_index,
+                                  columns=list(aws_df.index))
+        
+        AWS_RCM = pd.concat([AWS_RCM, sampled_df])
+
+        # Now sample the data in the remaining years: 
+        for year in range(date_range[0]+1, date_range[1]+1):
+            print(year)
+            # Open files and combine into an annual dataset:
+            RCM = load_dmidata(home_dir, year, rcm_name)
+            print('data loaded')
+            # Reshape melt data for compatibility with the lat-lon grid
+            melt_data = np.array(RCM.snmel.data).reshape(RCM.snmel.data.shape[0], -1)
+            
+            # Extract melt data for the nearest grid points
+            melt_data_aws = melt_data[:, closest_indices]
+
+            time_index = np.arange(np.datetime64(f'{year}-01-01'), 
+                                           np.datetime64(f'{year+1}-01-01'), 
+                                           np.timedelta64(1, 'D'))
+            
+            # Create a DataFrame with the melt data
+            sampled_df = pd.DataFrame(data=melt_data_aws, 
+                                  index=time_index,
+                                  columns=list(aws_df.index))
+            AWS_RCM = pd.concat([AWS_RCM, sampled_df])
+
+    return AWS_RCM
+
+
+def load_AWS_data(id): 
+    old_names = dict({'CP1': 'CrawfordPoint1', 'DY2': 'DYE-2', 'HUM': 'Humboldt', 'NAE': 'NASA-E', 'NAU': 'NASA-U', 
+                  'NEM': 'NEEM', 'NSE': 'NASA-SE','SDM': 'SouthDome', 'SDL': 'Saddle', 'TUN': 'Tunu-N', 
+                  'SWC': 'SwissCamp','Summit': 'Summit', 'PetermannELA': 'PetermannELA', 'GITS': 'GITS'})
+
+    # If the AWS station is part of the old gc-net when we want to open the historical data:
+    if id in ['CP1', 'DY2', 'HUM', 'NAE', 'NAU', 'NEM', 'NSA', 'NSE','SDM', 'SDL', 'TUN', 'SWC', 'Summit', 'PetermannELA', 'GITS']:
+        id = old_names[id]
+        # Load in AWS data:
+        AWS_data = pd.read_csv(home_dir + f'/AWS/combined/historical/{id}_daily.csv', header = 14,low_memory=False)
+        AWS_data= AWS_data.iloc[10:] # Remove first 10 rows where there no data. 
+        AWS_data = AWS_data.astype({'TA1': 'float', 'TA2': 'float', 'TA3': 'float', 'TA4': 'float'}) # Convert to float
+        
+        # Compute mean of TA1, TA2, TA3, TA4 and store in a new column named t_u
+        AWS_data['t_u'] = AWS_data[['TA1', 'TA2', 'TA3', 'TA4']].mean(axis=1)
+
+        AWS_data = AWS_data.rename(columns = {AWS_data.columns[0]: 'time'}) # rename the first column to time
+        #AWS_data.time = AWS_data.time.tz_localize(None) # Remove timezone info
+
+
+    else: 
+        # Load in AWS that part of PROMICE ogrinally:
+        AWS_data = pd.read_csv(home_dir + f'/AWS/combined/day/{id}_day.csv') # Load in AWS data
+    
+    # Convert to datetime and set time as index:
+    AWS_data['time'] = pd.to_datetime(AWS_data['time']) # Convert to datetime
+    AWS_data = AWS_data.set_index('time') # Set time as index
+    AWS_data.index = AWS_data.index.tz_localize(None) # remove tz 
+    # select only t_u: 
+    return AWS_data[['t_u']]
+
+
+def load_aws_locations(home_dir): 
+    # Load in AWS stations and mask applied to select AWS stations: 
+    aws_loc = pd.read_csv(home_dir + '/AWS/combined/AWS_latest_locations.csv')
+    ASCAT_mask =gpd.read_file(home_dir + '/QGIS/ASCAT_mask.geojson') 
+    # set station ID as index and remove stations where we don't have datafiles from:
+    aws_loc = aws_loc.set_index('stid')
+    aws_loc = aws_loc.drop(['QAS_Lv3','ZAK_L','WEG_L', 'KPC_Uv3', 'KPC_Lv3']) # remove nan entry and st where I dont have data. 
+
+    # Convert to geopandas dataframe and reproject to EPSG:3413:
+    gdf = gpd.GeoDataFrame(aws_loc, geometry=gpd.points_from_xy(aws_loc.lon, aws_loc.lat), crs="EPSG:4326").to_crs('EPSG:3413')
+
+    # Clip to ASCAT mask
+    clipped_aws_loc = gpd.clip(gdf, ASCAT_mask) 
+    return clipped_aws_loc
+
+def make_data_mask(home_dir):
+    # Load snowline and ascat scene to compte ascat mask:
+    snow_line = gpd.read_file(home_dir + '/snowline/2020_snowline_epsg3413.geojson')
+    ascat_scene = rxr.open_rasterio(home_dir + '/ASCAT/2020/meltmap_v02_2020-01-07.tif')
+
+    # Clip the ascat scene to the snowline:
+    masked_data = ascat_scene.rio.clip(snow_line.geometry, snow_line.crs, drop=False)
+
+    # convert the masked data to a numpy array dtype float:
+    mask= masked_data.to_numpy().astype('float64').squeeze()
+    # Where masked array is 0, set to nan:
+    mask[mask == 0] = 1
+    # where masked array is over zero, set to 1:
+    mask[mask > 1] = np.nan
+
+    # convert the ascat scene to a numpy array dtype float to get the unmasked data:
+    unmasked = ascat_scene.data.astype('float64').squeeze()
+    unmasked[unmasked == 0] = 1
+    # where masked array is over zero, set to 1:
+    unmasked[unmasked > 1] = np.nan
+
+    return mask, unmasked
